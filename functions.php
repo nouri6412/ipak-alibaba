167624d2c051cad4fcaea2167a7da566981241f4
Jalil Nouri <nouri.6412@gmail.com>
2021-11-27 16:13:57 +0330

    123

--- functions.php
+++ functions.php
@@ -10,6 +10,17 @@
  * @since 1.0.0
  */
 
+add_action( 'wp_enqueue_scripts', 'ipak_add_my_script' );
+function ipak_add_my_script() {
+    wp_enqueue_script(
+        'ipak-script', // name your script so that you can attach other scripts and de-register, etc.
+        get_template_directory_uri() . '/assets/js/menu.js', // this is the location of your script file
+        array('jquery') // this array lists the scripts upon which your script depends
+    );
+}
+
 require get_template_directory() . '/inc/menu.php';
+// require get_template_directory() . '/inc/wp_bootstrap_navwalker.php';
+
 
 
