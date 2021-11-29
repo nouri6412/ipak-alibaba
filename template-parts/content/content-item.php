cb212824b1bcbd29df3815d1a64b2d8fffb8f1ca
Jalil Nouri <nouri.6412@gmail.com>
2021-11-29 16:59:29 +0330

    123

--- template-parts/content/content-item.php
+++ template-parts/content/content-item.php
@@ -1,7 +1,7 @@
 <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 product-item">
     <div class="product-item-content img-zoom">
         <div class="product-item-content-img">
-            <a href="#"><?php the_post_thumbnail(); ?></a>
+            <a href="<?php echo esc_url(get_permalink());  ?>"><img src="<?php the_post_thumbnail_url(); ?>"" /></a>
         </div>
         <a href="<?php echo esc_url(get_permalink());  ?>">
             <h4 class="product-item-content-title">
